e8e0def60cddabfcc50ad8e176419f2c618e73be
Suyash00 <82312866+Suyash00@users.noreply.github.com>
2021-05-29 23:06:17 +0530

    footer

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-console.log(txtInput);
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
+function getTranslationURL(text) {
+    return (serverURL + "?text=" + text);
+}
 
-btnTranslate.addEventListener("click" ,function clickEventListener()
-{
-    outputDiv.innerText =" asdad" + txtInput.value;
-})
+// function errorHandler(error){
+//     console.log("error occured",error)
+//     alert("Something went wrong. try again")
+// }
+
+function clickHandler() {
+
+    var inputText = txtInput.value; //taking input
+    // //calling server for processsing
+    fetch(getTranslationURL(inputText))
+        .then(response => response.json())
+        .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText =  translatedText;
+            }
+        )
+
+}
+//  catch(errorHandler)
+
+// function clickHandler(){
+
+// }
+
+btnTranslate.addEventListener('click', clickHandler);
